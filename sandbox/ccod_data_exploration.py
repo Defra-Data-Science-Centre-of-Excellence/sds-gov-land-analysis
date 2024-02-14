16dca1b7a641c1f5373b5ccef4e0b5d144c3aa45
jasmine.elliott@defra.gov.uk <jasmine.elliott@defra.gov.uk>
2024-02-14 16:53:24 +0000

    successful extraction of relatively clean defra titles

--- sandbox/ccod_data_exploration.py
+++ sandbox/ccod_data_exploration.py
@@ -18,14 +18,14 @@ ccod = pd.read_csv(
     ccod_path,    
     usecols=[
         "Title Number",
+        "Tenure",
         "Proprietor Name (1)",
         "Company Registration No. (1)",
+        "Proprietorship Category (1)",
         "Proprietor (1) Address (1)",
-        ],
-    converters={
-        "Proprietor Name (1)": str.lower,
-        "Proprietor (1) Address (1)": str.lower,
-        }
+        "Date Proprietor Added",
+        "Additional Proprietor Indicator"
+        ]
 )
 
 ccod.head()
@@ -39,17 +39,47 @@ ccod_proprietor_categories
 
 # COMMAND ----------
 
-# find some clean defra data for reference
-ccod_defra = ccod.loc[ccod['Proprietor Name (1)'].str.contains("environment", na=False)]
-ccod_defra.head(30)
+# find some clean defra data for reference using single string method
+ccod_defra = ccod.loc[ccod['Proprietor Name (1)'].str.contains('secretary of state for environment', case=False, na=False)]
+print(ccod_defra.size)
+ccod_defra.head(n=50)
 
 # COMMAND ----------
 
-# read in national polygon service data
+# find some clean defra data for reference using 'and' method
+ccod_defra = ccod.loc[ccod['Proprietor Name (1)'].str.contains(r'^(?=.*secretary)(?=.*state)(?=.*environment)', case=False, na=False)]
+print(ccod_defra.size)
+ccod_defra.head(n=50)
 
 # COMMAND ----------
 
+# find some defra data for reference using 'or' method
+ccod_filtered = ccod.loc[ccod['Proprietor Name (1)'].str.contains('|'.join(['secretary','state','minister','ministry','deprtment']), case=False, na=False)]
+ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietor Name (1)'].str.contains('|'.join(['environment','food','rural']), case=False, na=False)]
+ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietorship Category (1)'].str.contains('Corporate Body', case=False, na=False)]
+print(ccod_filtered.size)
+print(ccod_filtered['Proprietor Name (1)'].unique())
+ccod_filtered.head(20)
 
+# COMMAND ----------
+
+# find some environment agency data for reference using 'or' method
+ccod_filtered = ccod.loc[ccod['Proprietor Name (1)'].str.contains('|'.join(['agency']), case=False, na=False)]
+ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietor Name (1)'].str.contains('|'.join(['environment']), case=False, na=False)]
+ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietorship Category (1)'].str.contains('Corporate Body', case=False, na=False)]
+print(ccod_filtered.size)
+print(ccod_filtered['Proprietor Name (1)'].unique())
+ccod_filtered.head(20)
+
+# COMMAND ----------
+
+# find some natural england data for reference using 'or' method
+ccod_filtered = ccod.loc[ccod['Proprietor Name (1)'].str.contains('|'.join(['natural']), case=False, na=False)]
+ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietor Name (1)'].str.contains('|'.join(['england']), case=False, na=False)]
+ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietorship Category (1)'].str.contains('Corporate Body', case=False, na=False)]
+print(ccod_filtered.size)
+print(ccod_filtered['Proprietor Name (1)'].unique())
+ccod_filtered.head(20)
 
 # COMMAND ----------
 
