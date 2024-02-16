52f9bd52db7e511abaa792559c7d22c5750b4281
jasmine.elliott@defra.gov.uk <jasmine.elliott@defra.gov.uk>
2024-02-16 17:29:29 +0000

    identifying ALB names in Proprietor fields

--- sandbox/ccod_data_exploration.py
+++ sandbox/ccod_data_exploration.py
@@ -39,37 +39,137 @@ ccod_proprietor_categories
 
 # COMMAND ----------
 
-# find some clean defra data for reference using single string method
-ccod_defra = ccod.loc[ccod['Proprietor Name (1)'].str.contains('secretary of state for environment', case=False, na=False)]
-print(ccod_defra.size)
-ccod_defra.head(n=50)
-
-# COMMAND ----------
-
-# find some clean defra data for reference using 'and' method
-ccod_defra = ccod.loc[ccod['Proprietor Name (1)'].str.contains(r'^(?=.*secretary)(?=.*state)(?=.*environment)', case=False, na=False)]
-print(ccod_defra.size)
-ccod_defra.head(n=50)
-
-# COMMAND ----------
-
-# find some defra data for reference using 'or' method
-ccod_filtered = ccod.loc[ccod['Proprietor Name (1)'].str.contains('|'.join(['secretary','state','minister','ministry','deprtment']), case=False, na=False)]
+import numpy as np
+# find some defra data for reference using or-and-or method
+ccod_filtered = ccod.loc[ccod['Proprietor Name (1)'].str.contains('|'.join(['secretary','state','minister','ministry','department']), case=False, na=False)]
 ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietor Name (1)'].str.contains('|'.join(['environment','food','rural']), case=False, na=False)]
 ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietorship Category (1)'].str.contains('Corporate Body', case=False, na=False)]
 print(ccod_filtered.size)
+display(ccod_filtered['Proprietor Name (1)'].unique())
+#np.savetxt('/dbfs/mnt/lab/restricted/ESD-Project/jasmine.elliott@defra.gov.uk/defra_names.csv',ccod_filtered['Proprietor Name (1)'].unique(), delimiter=',', fmt='%s')
+#csv = pd.read_csv('/dbfs/mnt/lab/restricted/ESD-Project/jasmine.elliott@defra.gov.uk/defra_names.csv', sep=',')
+#display(csv)
+
+# COMMAND ----------
+
+# find some environment agency data for reference using or-and-or method
+ccod_filtered = ccod.loc[ccod['Proprietor Name (1)'].str.contains('|'.join(['agency']), case=False, na=False)]
+ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietor Name (1)'].str.contains('|'.join(['environment']), case=False, na=False)]
+# Should probably display Proprietor names removed due to corporate body filter
+ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietorship Category (1)'].str.contains('Corporate Body', case=False, na=False)]
+print(ccod_filtered.size)
 print(ccod_filtered['Proprietor Name (1)'].unique())
 ccod_filtered.head(20)
 
 # COMMAND ----------
 
-# find some environment agency data for reference using 'or' method
-ccod_filtered = ccod.loc[ccod['Proprietor Name (1)'].str.contains('|'.join(['agency']), case=False, na=False)]
-ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietor Name (1)'].str.contains('|'.join(['environment']), case=False, na=False)]
-ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietorship Category (1)'].str.contains('Corporate Body', case=False, na=False)]
-print(ccod_filtered.size)
-print(ccod_filtered['Proprietor Name (1)'].unique())
-ccod_filtered.head(20)
+# list of ALB/department names of interest - construct dict where values are alternative name options?
+cs_department_identifiers = ['state', 'secretary', 'ministry', 'minister', 'department']
+cs_department_names = {
+    'Department for Environment, Food and Rural Affairs':
+        ['environement', 'food', 'rural']
+}
+alb_names = {
+    'Natural England': ['English Nature', 'Countryside Agency', 'Rural Development Service', 'Nature Conservancy Council'],
+    'Joint Nature Conservation Committee': [],
+    'Environment Agency': [],
+    'Rural Payments Agency': [],
+    'Royal Botanic Gardens, Kew': [],
+    'Agriculture and Horiculture Development Board': [],
+    'Animal, Plant Health Agency': [],
+    'Marine Management Organisation': [],
+    'Forestry Commission': ['Forestry England'],
+}
+
+# COMMAND ----------
+
+cs_department_translation_dict = {
+    'Department for Environment, Food and Rural Affairs':
+}
+
+# COMMAND ----------
+
+alb_found_names_translation_dict = {
+    'Natural England':{
+        'Natural England': [],
+        'English Nature': [],
+        'Countryside Agency': [],
+        'Rural Development Service': [],
+        'Nature Conservancy Council': [],
+    },
+    'Joint Nature Conservation Committee':{
+        'Joint Nature Conservation Committee': [],
+    },
+    'Environment Agency':{
+        'Environment Agency': [],
+    },
+    'Rural Payments Agency':{
+        'Rural Payments Agency': [],
+    },
+    'Royal Botanic Gardens Kew':{
+        'Royal Botanic Gardens, Kew': [],
+    },
+    'Agriculture and Horiculture Development Board':{
+        'Agriculture and Horiculture Development Board': [],
+    },
+    'Animal Plant Health Agency':{
+        'Animal, Plant Health Agency': [],
+    },
+    'Marine Management Organisation':{
+        'Marine Management Organisation': [],
+    },
+    'Forestry Commission':{
+        'Forestry Commission': [],
+        'Forestry England': [],
+        'Forestry Research': [],
+    },
+}
+
+# COMMAND ----------
+
+# find likely names to populate translation dict
+regex_str = r'^{}'
+expression = '(?=.*{})'
+for current_org, org_names in alb_found_names_translation_dict.items():
+    for org_name in org_names:
+        if org_name != '':
+            # identify titles registered under current name and tag with current and current organisation
+            compiled_regex_str = regex_str.format(''.join(expression.format(word) for word in org_name.split(' ')))
+            ccod_filtered = ccod[ccod['Proprietor Name (1)'].str.contains(f'{compiled_regex_str}', case=False, na=False)]
+            found_names = ccod_filtered['Proprietor Name (1)'].unique()
+            alb_found_names_translation_dict[current_org][org_name] = found_names.tolist()
+            # identify titles registed under historic names and tag with current and historic organisations
+print(alb_found_names_translation_dict)
+
+# COMMAND ----------
+
+# produce df from translation dict for manual QA - this could be exported to csv for editing if needed
+alb_found_names_translation_df = pd.DataFrame(alb_found_names_translation_dict)
+alb_found_names_translation_df.head(20)
+
+
+# COMMAND ----------
+
+# find some environment agency data for reference using or-and-or method
+regex_str = r'^{}'
+expression = '(?=.*{})'
+for current_org, org_names in alb_names.items():
+    # identify titles registered under current name and tag with current and current organisation
+    compiled_regex_str = regex_str.format(''.join(expression.format(word) for word in current_org.split(' ')))
+    ccod.loc[ccod['Proprietor Name (1)'].str.contains(f'{compiled_regex_str}', case=False, na=False),'organisation']=current_org
+    ccod.loc[ccod['Proprietor Name (1)'].str.contains(f'{compiled_regex_str}', case=False, na=False),'current_organisation']=current_org
+    # identify titles registed under historic names and tag with current and historic organisations
+    for historic_org in org_names:
+        compiled_regex_str = regex_str.format(''.join(expression.format(word) for word in historic_org.split(' ')))
+        ccod.loc[ccod['Proprietor Name (1)'].str.contains(f'{compiled_regex_str}', case=False, na=False),'organisation']=historic_org
+        ccod.loc[ccod['Proprietor Name (1)'].str.contains(f'{compiled_regex_str}', case=False, na=False),'current_organisation']=current_org
+ccod[ccod['organisation'].notnull()].head()  
+
+
+# COMMAND ----------
+
+ccod_identified = ccod[ccod['organisation'].notnull()]
+print(ccod_identified['Proprietor Name (1)'].unique())
 
 # COMMAND ----------
 
@@ -80,20 +180,6 @@ ccod_filtered = ccod_filtered.loc[ccod_filtered['Proprietorship Category (1)'].s
 print(ccod_filtered.size)
 print(ccod_filtered['Proprietor Name (1)'].unique())
 ccod_filtered.head(20)
-
-# COMMAND ----------
-
-# list of ALB/department names of interest - construct dict where values are alternative name options?
-alb_names = [
-    'Natural England',
-    'Joint Nature Conservation Committee',
-    'Environment Agency',
-    'Rural Payments Agency',
-    'Royal Botanic Gardens, Kew',
-    'Agriculture and Horiculture Development Board'
-    'Animal, Plant Health Agency',
-    'Marine Management Organisation'
-]
 
 # COMMAND ----------
 
