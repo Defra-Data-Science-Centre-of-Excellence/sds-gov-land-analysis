41cf8a9e419396aed4e87a8ee79ca8ad2b986dbe
jasmine.elliott@defra.gov.uk <jasmine.elliott@defra.gov.uk>
2024-03-08 15:08:39 +0000

    cross notebook time-saving tools

---
+++ sandbox/paths.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### Input data
+
+# COMMAND ----------
+
+ccod_path = '/dbfs/mnt/lab/restricted/ESD-Project/source_data_gov_hm_land_registry/dataset_use_land_and_property_data/format_CSV_use_land_and_property_data/LATEST_use_land_and_property_data/CCOD_FULL_2024_01.csv'
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Output data
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Existing data for comparison
+
+# COMMAND ----------
+
+ea_titles_path = "/dbfs/mnt/lab/restricted/ESD-Project/jasmine.elliott@defra.gov.uk/gov_land_analysis/existing_data_for_comparison/EA_Freehold_titles.xlsx"
