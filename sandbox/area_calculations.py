190d9e55d107834c16b465cbe269be82a1994a62
jasmine.elliott@defra.gov.uk <jasmine.elliott@defra.gov.uk>
2024-05-24 14:36:37 +0000

    adding area calculations

--- sandbox/area_calculations.py
+++ sandbox/area_calculations.py
@@ -6,3 +6,33 @@ import pandas as pd
 
 # MAGIC %run
 # MAGIC ./paths
+
+# COMMAND ----------
+
+# MAGIC %run
+# MAGIC ./constants
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Set organisations to calculate area for
+
+# COMMAND ----------
+
+organisations_of_interest = alb_found_names_translation_dict.keys()
+
+
+# COMMAND ----------
+
+import defra ccod-polygon data
+
+# COMMAND ----------
+
+#For each organisation of interest
+#filter by organisation
+#dissolve
+#get area
+#filter by leasehold
+#get area
+#filter by freehold
+#get area
